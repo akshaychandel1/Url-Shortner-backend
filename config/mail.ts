f4b48c01da7587fc912a142b11459925b0b87528
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-07-22 16:01:24 +0530

    submit mail.ts

--- config/mail.ts
+++ config/mail.ts
@@ -20,19 +20,26 @@
 
 
 
-
-
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// Node ko globally bol do ki sabse pehle IPv4 resolve kare
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // Port 587 ke liye false (STARTTLS)
+  secure: false,
   auth: {
     user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD, // Must be an 16-character App Password
+    pass: process.env.SMTP_PASSWORD,
   },
-  family: 4, // Bypasses IPv6 issue on Render
-} as nodemailer.TransportOptions);
+  // Strict typing workaround for nodemailer lookup
+  lookup: (hostname, _options, callback) => {
+    dns.lookup(hostname, 4, (err, address, family) => {
+      callback(err, address, family);
+    });
+  },
+} as any);
 
 export default transporter;
